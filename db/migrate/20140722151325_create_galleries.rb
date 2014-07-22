b59fb06027d2a3999c9190d84ef27ec1fb0341ae
Prelang Builder <builder@prelang.com>
2014-07-22 15:13:25 +0000

    Generated model Gallery with migration

---
+++ db/migrate/20140722151325_create_galleries.rb
@@ -0,0 +1,12 @@
+class CreateGalleries < ActiveRecord::Migration
+  def change
+    create_table :galleries do |t|
+      t.string :image
+      t.string :title
+      t.string :category
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
